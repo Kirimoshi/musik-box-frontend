ddec4203d281f3e73bbd773a1aba362140552370
Saiprakash Chiluka <saiprakash_chiluka@epam.com>
2023-06-22 14:02:18 +0530

    202-included-routes-for-signin-signup

--- My-Music-App/src/App.js
+++ My-Music-App/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "./SignUp/style.css";
 import { SignUp } from "./SignUp/components/SignUp";
+import { Login } from "./SignIn/components/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<h1>Home</h1>}/>
           <Route path="/SignUp" element={<SignUp />} />
-          <Route path="/SignIn" />
+          <Route path="/SignIn" element={<Login/>}/>
         </Routes>
       </div>
     </BrowserRouter>
