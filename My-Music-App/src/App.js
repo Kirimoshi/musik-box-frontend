7194fea1274469577d96b0a00e74fedede0baaa1
Bhaskara Vanacharla <bhaskara_vanacharla@epam.com>
2023-06-27 00:16:20 +0530

    221-updated-app-file

--- My-Music-App/src/App.js
+++ My-Music-App/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { SignUp } from "./SignUp/components/SignUp";
 import { Login } from "./SignIn/components/Login";
-import {MyPlayListPage} from "./ViewThePlayList/Components/MyPlayListPage"
+import MyPlayListPage from "./ViewThePlayList/Components/MyPlayListPage"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
