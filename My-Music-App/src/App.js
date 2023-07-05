415868ff00bb494d95410495b34d806d893985a7
Bhaskara Vanacharla <bhaskara_vanacharla@epam.com>
2023-07-06 00:59:02 +0530

    344-added-all-the-files

--- My-Music-App/src/App.js
+++ My-Music-App/src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { SignUp } from "./SignUp/components/SignUp";
 import { Login } from "./SignIn/components/Login";
+import MyPlayListPage from './ViewMyPlayLists/components/MyPlayListPage';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
           <Route path="/" element={<h1>Home</h1>}/>
-          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/SignUp" element={<SignUp />} /> 
           <Route path="/SignIn" element={<Login/>}/>
+          <Route path="/viewmyplaylists" element={<MyPlayListPage/>}/>
         </Routes>
       </div>
     </BrowserRouter>
