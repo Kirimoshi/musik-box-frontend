fe317d068254f2521b30a5efadec64b91cf7d4ed
Saiprakash Chiluka <saiprakash_chiluka@epam.com>
2023-07-07 22:50:36 +0530

    344-resolved-merge-conflicts

--- My-Music-App/src/SignIn/components/Login.jsx
+++ My-Music-App/src/SignIn/components/Login.jsx
@@ -5,7 +5,6 @@ import { useFormik } from "formik";
 import { SignInSchema } from "../schemas/SignInSchema";
 import { BsFillExclamationCircleFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
-import { Link } from "react-router-dom";
 import axios from "axios";
 import { constants } from "../constants";
 import { useNavigate } from "react-router-dom";
@@ -33,7 +32,6 @@ export const Login = (props) => {
     isValid,
     setFieldValue,
     setFieldError,
-    setFieldError,
   } = useFormik({
     initialValues: {
       email: "",
@@ -46,7 +44,6 @@ export const Login = (props) => {
   const clearFunc = (name) => {
     setFieldValue(name, "");
   };
-  const post = () => {
   const post = () => {
     const userData = {
       email: values.email,
@@ -84,27 +81,13 @@ export const Login = (props) => {
         } else {
           errors.email = response.errors;
           setFieldError(errors.email);
-        console.log(error);
-        const response = error.response.data;
-        console.log(response.errors);
-        if (response.errors === "Invalid password") {
-          errors.password = response.errors;
-          setFieldError(errors.password);
-          console.log(errors.password);
-        } else {
-          errors.email = response.errors;
-          setFieldError(errors.email);
         }
       });
-  };
   };
 
   return (
     <div className="header-container">
       <div className="header">
-        <span className="header-content" data-testid="signin">
-          Sign In
-        </span>
         <span className="header-content" data-testid="signin">
           Sign In
         </span>
@@ -148,65 +131,7 @@ export const Login = (props) => {
                     />
                   )}
                 </div>
-        <form className="login-form" onSubmit={handleSubmit} autoComplete="off">
-          <div className="login-form-inputcheckbox">
-            <div className="inputs">
-              <div className="label_div_ip1">
-                <div
-                  className={`input-wrapper ${
-                    errors.email && touched.email ? "ip-error" : ""
-                  }`}
-                >
-                  <label
-                    htmlFor="inputbox"
-                    className={
-                      errors.email && touched.email
-                        ? "label label-error"
-                        : "label"
-                    }
-                  >
-                    Email
-                  </label>
-                  <input
-                    value={values.email}
-                    onChange={handleChange}
-                    className="inputbox"
-                    type="email"
-                    name="email"
-                    onBlur={handleBlur}
-                    data-testid="emailtest"
-                  />
-                  {errors.email && touched.email ? (
-                    <BsFillExclamationCircleFill className="exclamation-circle" />
-                  ) : (
-                    <AiOutlineCloseCircle
-                      className="close-circle"
-                      data-testid="closetest1"
-                      onClick={() => clearFunc("email")}
-                    />
-                  )}
-                </div>
 
-                {errors.email && touched.email && (
-                  <p className="error">{errors.email}</p>
-                )}
-              </div>
-              <div className="label_div_ip1">
-                <div
-                  className={`input-wrapper ${
-                    errors.password && touched.password ? "ip-error" : ""
-                  }`}
-                >
-                  <label
-                    htmlFor="password"
-                    className={
-                      errors.password && touched.password
-                        ? "label label-error"
-                        : "label"
-                    }
-                  >
-                    Password
-                  </label>
                 {errors.email && touched.email && (
                   <p className="error">{errors.email}</p>
                 )}
@@ -237,15 +162,6 @@ export const Login = (props) => {
                     name="password"
                     data-testid="passwordtest"
                   />
-                  <input
-                    className="inputbox"
-                    id="password-label"
-                    value={values.password}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    name="password"
-                    data-testid="passwordtest"
-                  />
 
                   {errors.password && touched.password ? (
                     <BsFillExclamationCircleFill className="exclamation-circle" />
@@ -257,22 +173,7 @@ export const Login = (props) => {
                     />
                   )}
                 </div>
-                  {errors.password && touched.password ? (
-                    <BsFillExclamationCircleFill className="exclamation-circle" />
-                  ) : (
-                    <AiOutlineCloseCircle
-                      className="close-circle"
-                      data-testid="closetest2"
-                      onClick={() => clearFunc("password")}
-                    />
-                  )}
-                </div>
 
-                {errors.password && touched.password && (
-                  <p className="error">{errors.password}</p>
-                )}
-              </div>
-            </div>
                 {errors.password && touched.password && (
                   <p className="error">{errors.password}</p>
                 )}
@@ -297,20 +198,7 @@ export const Login = (props) => {
               </label>
             </div>
           </div>
-              <label className="checkbox-text" htmlFor="checkbox">
-                Remember me
-              </label>
-            </div>
-          </div>
 
-          <div className="sigin-sigup">
-            <button
-              className={`signin-button ${!isValid ? "signin-error" : ""}`}
-              type="submit"
-              onClick={post}
-            >
-              Sign In
-            </button>
           <div className="sigin-sigup">
             <button
               className={`signin-button ${!isValid ? "signin-error" : ""}`}
@@ -322,17 +210,7 @@ export const Login = (props) => {
 
             <div className="signin-signup-text">
               <span className="p">Don't have a account yet?</span>
-            <div className="signin-signup-text">
-              <span className="p">Don't have a account yet?</span>
 
-              <nav>
-                <Link to="/SignUp" className="signup">
-                  Sign Up
-                </Link>
-              </nav>
-            </div>
-          </div>
-        </form>
               <nav>
                 <Link to="/SignUp" className="signup">
                   Sign Up
